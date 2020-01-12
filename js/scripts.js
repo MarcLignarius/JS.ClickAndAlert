54ef1d4805fe05fd0ced71d5b5cc52d4f1f34842
Marc Davies <marcdaviesriot@gmail.com>
2020-01-12 14:48:34 -0800

    refactor code to use forEach() loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
+// $(function() {
+//   $("h1").click(function() {
+//   alert("This is a header.");
+//   alert("I told you, THIS IS A HEADER!");
+//   });
+
+//   $("p").click(function() {
+//   alert("This is a paragraph.");
+//   });
+
+//   $("img").click(function() {
+//   alert("This is an image.");
+//   });
+// });
+
 $(function() {
-  $("h1").click(function() {
-  alert("This is a header.");
-  alert("I told you, THIS IS A HEADER!");
-  });
-
-  $("p").click(function() {
-  alert("This is a paragraph.");
-  });
-
-  $("img").click(function() {
-  alert("This is an image.");
-  });
-});
+  var elements = ['h1' ,'p', 'img'];
+  elements.forEach(function(element) {
+    $(element).click(function() {
+      alert("This element is wrapped in a <" + element + "> tag.");
+    })
+  })
+})
 
 
 
